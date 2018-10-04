332948efa23cad51289afb5e170ac94e30752725
eerdoga <eerdoga@purdue.edu>
2018-10-04 17:16:50 -0400

    Finished Lab

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -11,18 +11,29 @@ public class StringUtils {
         /* Generate the long version of the name first:
             Ex: firstName = "Sean", lastName = "Flannery"
                     --> res = "SFlannery"
+
          */
+
         String res = firstName.substring(0,1) + lastName;
         /* Shorten the string to 8 characters
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
+
          */
-        res.substring(0, 8);
+        int length = lastName.length();
+        if (length>8) {
+            length = 7;
+        }
+        else{
+            length = length;
+
+        }
+        res = res.substring(0, length+1);
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        res = res.toLowerCase();
         /* return the result of our calculation */
         return res;
     }
@@ -36,6 +47,6 @@ public class StringUtils {
      */
     public static String replaceStudentUsername(String text, String username) {
         /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+        return text.replaceAll(username, "[DATA EXPUNGED]");
     }
 }
